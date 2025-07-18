5db8f21392320d653f7a4a24e6e7143d995cb2b8
Manjunath063 <manjunathjangam198@gmail.com>
2025-07-18 16:41:31 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -17,13 +17,13 @@ const pool = new Pool({
 
 // Middleware
 app.use(cors({
-    origin: ['http://13.60.169.140:3081', 
+    origin: ['http://16.171.154.98:3081', 
         'http://127.0.0.1:5500',
          'http://localhost:5500',
          'http://127.0.0.1:5502',
          'http://127.0.0.1:5503',
-          'http://13.60.169.140:8162',
-          'http://13.60.169.140:8163',
+          'http://16.171.154.98:8162',
+          'http://16.171.154.98:8163',
         ],
     methods: ['GET', 'POST', 'PUT', 'OPTIONS'],
     allowedHeaders: ['Content-Type'],
@@ -186,9 +186,9 @@ app.use((err, req, res, next) => {
 // Start server
 testDatabaseConnection().then(() => {
     app.listen(port, '0.0.0.0', () => {
-        console.log(`Server running on http://13.60.169.140:${port}`);
-        console.log(`Employee Dashboard: http://13.60.169.140:${port}/`);
-        console.log(`HR Dashboard: http://13.60.169.140:${port}/hr`);
+        console.log(`Server running on http://16.171.154.98:${port}`);
+        console.log(`Employee Dashboard: http://16.171.154.98:${port}/`);
+        console.log(`HR Dashboard: http://16.171.154.98:${port}/hr`);
     });
 }).catch(err => {
     console.error('Failed to start server:', err.message);
